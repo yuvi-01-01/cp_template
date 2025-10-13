a54125b4c4d8b0c6731a17e0c8515373d23e73e3
yuvi-01-01 <164642105+yuvi-01-01@users.noreply.github.com>
2025-10-13 20:32:38 +0530

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,1 +1,30 @@
-
+class BIT {
+public:
+    int n;
+    vl tree;
+    BIT(int size){
+        n=size+2; //safe
+        tree.assign(n,0);
+    }
+    void update(int idx,ll delta){
+        while(idx<n){
+            tree[idx] += delta;
+            idx += rsb(idx);
+        }
+    }
+    ll presum(int idx){
+        ll res=0;
+        while(idx > 0){
+            res += tree[idx];
+            idx -= rsb(idx);
+        }
+        return res;
+    }
+    ll query(int l,int r){
+        return presum(r)-presum(l-1);
+    }
+    int rsb(int i){
+        return (i & (-i));
+    }
+    
+};
